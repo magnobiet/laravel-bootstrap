56b21a86d9cf5298baef7239ff432ce37600b155
Magno Biét <magno.biet@gmail.com>
2018-04-27 13:59:51 -0300

    Add brazilian States and Cities
    https://github.com/magnobiet/laravel-states-cities-brazil

---
+++ app/Entities/State.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class State extends Model
+{
+
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'abbr',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at',
+    ];
+
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = [
+        'deleted_at'
+    ];
+
+    /**
+     * Get all of the cities for the state.
+     */
+    public function city()
+    {
+
+        return $this->hasMany(City::class);
+
+    }
+
+}
