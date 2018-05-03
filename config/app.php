293506b314c473f1cfb77ac06fc8d9c4e69710ee
Magno Biét <magno.biet@gmail.com>
2018-05-03 17:32:00 -0300

    Add Cloudinary intergration
    https://github.com/jrm2k6/cloudder

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
         OwenIt\Auditing\AuditingServiceProvider::class,
         Barryvdh\Debugbar\ServiceProvider::class,
         JeroenNoten\LaravelAdminLte\ServiceProvider::class,
+        JD\Cloudder\CloudderServiceProvider::class,
 
     ],
 
@@ -218,6 +219,8 @@ return [
         'URL'          => Illuminate\Support\Facades\URL::class,
         'Validator'    => Illuminate\Support\Facades\Validator::class,
         'View'         => Illuminate\Support\Facades\View::class,
+
+        'Cloudder' => JD\Cloudder\Facades\Cloudder::class,
 
     ],
 
