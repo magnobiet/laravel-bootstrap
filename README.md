545aacae96a934f86553ee56897863b9e3c78bb9
Magno Biét <magno.biet@gmail.com>
2018-05-14 20:43:12 -0300

    Prepare to Heroku
    - Add `app.json`
    - Change default database to PostgreSQL
    - Add Deploy Button to `README.md` and section "Getting started"

--- README.md
+++ README.md
@@ -2,7 +2,21 @@
 
 ![Laravel Bootstrap screen shot](screenshot.png)
 
-## Getting Started
+## Getting started
+
+```bash
+git clone https://github.com/magnobiet/laravel-bootstrap.git && cd $_
+rm -rf .git/
+
+composer install
+
+php artisan app:name YourAppNamespace
+php artisan migrate --seed
+php artisan route:cache
+php artisan config:cache
+
+npm install
+```
 
 ## Installed packages
 
@@ -16,7 +30,11 @@
 - [barryvdh/laravel-debugbar](https://github.com/barryvdh/laravel-debugbar)
 - [laracasts/generators](https://github.com/laracasts/laravel-5-generators-extended)
 
-## Security Vulnerabilities
+## Deploy
+
+[![Deploy](https://www.herokucdn.com/deploy/button.svg)](https://heroku.com/deploy)
+
+## Security vulnerabilities
 
 If you discover a security vulnerability within this project, please send an e-mail to [magno.biet@gmail.com](mailto:magno.biet@gmail.com). All security vulnerabilities will be promptly addressed.
 
--- config/database.php
+++ config/database.php
@@ -1,5 +1,7 @@
 <?php
 
+$database_url = parse_url(env('DB_URL'));
+
 return [
 
     /*
@@ -34,48 +36,48 @@ return [
     'connections' => [
 
         'sqlite' => [
-            'driver' => 'sqlite',
+            'driver'   => 'sqlite',
             'database' => env('DB_DATABASE', database_path('database.sqlite')),
-            'prefix' => '',
+            'prefix'   => '',
         ],
 
         'mysql' => [
-            'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'driver'      => 'mysql',
+            'host'        => env('DB_HOST', $database_url['host']),
+            'port'        => env('DB_PORT', $database_url['port']),
+            'database'    => env('DB_DATABASE', substr($database_url['path'], 1)),
+            'username'    => env('DB_USERNAME', $database_url['user']),
+            'password'    => env('DB_PASSWORD', $database_url['pass']),
             'unix_socket' => env('DB_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'strict' => true,
-            'engine' => null,
+            'charset'     => 'utf8mb4',
+            'collation'   => 'utf8mb4_unicode_ci',
+            'prefix'      => '',
+            'strict'      => true,
+            'engine'      => null,
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', $database_url['host']),
+            'port'     => env('DB_PORT', $database_url['port']),
+            'database' => env('DB_DATABASE', substr($database_url['path'], 1)),
+            'username' => env('DB_USERNAME', $database_url['user']),
+            'password' => env('DB_PASSWORD', $database_url['pass']),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
         ],
 
         'sqlsrv' => [
-            'driver' => 'sqlsrv',
-            'host' => env('DB_HOST', 'localhost'),
-            'port' => env('DB_PORT', '1433'),
+            'driver'   => 'sqlsrv',
+            'host'     => env('DB_HOST', 'localhost'),
+            'port'     => env('DB_PORT', '1433'),
             'database' => env('DB_DATABASE', 'forge'),
             'username' => env('DB_USERNAME', 'forge'),
             'password' => env('DB_PASSWORD', ''),
-            'charset' => 'utf8',
-            'prefix' => '',
+            'charset'  => 'utf8',
+            'prefix'   => '',
         ],
 
     ],
@@ -109,9 +111,9 @@ return [
         'client' => 'predis',
 
         'default' => [
-            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'host'     => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD', null),
-            'port' => env('REDIS_PORT', 6379),
+            'port'     => env('REDIS_PORT', 6379),
             'database' => 0,
         ],
 
